259f05b61c5dfbf6bce618f32441e9bc6db0948c
JokkerKing0411 <62717332+JokkerKing0411@users.noreply.github.com>
2020-05-25 20:01:51 +0500

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -9,37 +9,10 @@ from telegraph import upload_file
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Salom {message.from_user.first_name},\nBu telegram dan telegra.ph ga rasm yuklab beruvchi bot\n Yaratuvchi @JokkerKing",
+        text=f"Salom {message.from_user.first_name},\nBu telegram dan telegra.ph ga Video yuklab beruvchi bot\n Yaratuvchi @JokkerKing",
         reply_to_message_id=message.message_id
     )
     
-@Client.on_message(Filters.photo)
-async def getimage(client, message):
-    location = "./FILES"
-    if not os.path.isdir(location):
-        os.makedirs(location)
-    imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
-    dwn = await client.send_message(
-          text="biroz kuting...",
-          chat_id = message.chat.id,
-          reply_to_message_id=message.message_id
-          )          
-    await client.download_media(
-            message=message,
-            file_name=imgdir
-        )
-    await dwn.edit_text("yuklanmoqda...")
-    try:
-        response = upload_file(imgdir)
-    except Exception as error:
-        await dwn.edit_text(f"xatolik yuz berdi\n{error}")
-        return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
-    try:
-        os.remove(imgdir)
-    except:
-        pass
-
 @Client.on_message(Filters.video)
 async def getvideo(client, message):
     location = "./FILES"
