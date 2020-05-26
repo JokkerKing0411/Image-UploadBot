0f5069bde6524cae3c280f7388ff4b11370630ff
JokkerKing0411 <62717332+JokkerKing0411@users.noreply.github.com>
2020-05-26 11:07:27 +0500

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -9,36 +9,35 @@ from telegraph import upload_file
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Salom {message.from_user.first_name},\nBu telegram dan telegra.ph ga Video yuklab beruvchi bot\n Yaratuvchi @JokkerKing",
+        text=f"🙋Salom {message.from_user.first_name},\n🤖Bu Bot telegram dan telegra.ph 🌃Rasm Yuklab beradi.\n🎓Yaratuvchi: @JokkerKing",
         reply_to_message_id=message.message_id
     )
     
-@Client.on_message(Filters.video)
-async def getvideo(client, message):
+@Client.on_message(Filters.photo)
+async def getimage(client, message):
     location = "./FILES"
     if not os.path.isdir(location):
         os.makedirs(location)
-    viddir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
+    imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
     dwn = await client.send_message(
-          text="biroz kuting...",
+          text="Yuklanmoqda...",
           chat_id = message.chat.id,
           reply_to_message_id=message.message_id
           )          
     await client.download_media(
             message=message,
-            file_name=viddir
+            file_name=imgdir
         )
-    await dwn.edit_text("yuklanmoqda...")
+    await dwn.edit_text("Biroz kuting...")
     try:
-        response = upload_file(viddir)
+        response = upload_file(imgdir)
     except Exception as error:
-        await dwn.edit_text(f"xatolik yuz berdi\n{error}")
+        await dwn.edit_text(f"Xatolik yuz berdi\n{error}")
         return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    await dwn.edit_text(f"https://telegra.ph{response[0]}\n\n🌃Image Upload By @Teleg2GraphBot")
     try:
-        os.remove(viddir)
+        os.remove(imgdir)
     except:
         pass
-
 
 
